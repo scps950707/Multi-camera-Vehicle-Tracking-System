09708b59ac39c04d5575d82f73cc255bdffe4521
scps950707 <scps950707@gmail.com>
2016-06-01 18:07:53 +0800

    remove unused headers

--- BackgroundSubtractor/backgroundSubtractor.cpp
+++ BackgroundSubtractor/backgroundSubtractor.cpp
@@ -1,7 +1,4 @@
 #include <opencv2/core/core.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/features2d/features2d.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/video/background_segm.hpp>
 using namespace cv;
