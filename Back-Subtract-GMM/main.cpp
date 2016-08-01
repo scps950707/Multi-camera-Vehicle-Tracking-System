2285041030f21300b0e7b59dab9f38aa7a0d5a00
scps950707 <scps950707@gmail.com>
2016-08-01 21:21:22 +0800

    variable rename and change blur method in main.cpp

--- Back-Subtract-GMM/main.cpp
+++ Back-Subtract-GMM/main.cpp
@@ -34,7 +34,7 @@ int main( int argc, char *argv[] )
         {"time", required_argument, NULL, 't'},
         {NULL, 0, NULL, 0}
     };
-    while ( ( options = getopt_long( argc, argv, "i:m:v:", long_opt, NULL ) ) != -1 )
+    while ( ( options = getopt_long( argc, argv, "i:m:v:t:", long_opt, NULL ) ) != -1 )
     {
         switch  ( options )
         {
@@ -58,7 +58,7 @@ int main( int argc, char *argv[] )
     // }}}
     // declare mat for input and mask
 
-    cv::Mat inputImg, outputImg;
+    cv::Mat inputImg, outputMask;
     cv::VideoCapture capture( inputPath );
     // perform fast foward
     capture.set( CV_CAP_PROP_POS_FRAMES, fastforward * FPS );
@@ -71,7 +71,7 @@ int main( int argc, char *argv[] )
     {
         cv::resize( inputImg, inputImg, cv::Size( inputImg.cols / 2, inputImg.rows / 2 ) );
     }
-    outputImg = cv::Mat( inputImg.rows, inputImg.cols, CV_8UC1, cv::Scalar( 0 ) );
+    outputMask = cv::Mat( inputImg.rows, inputImg.cols, CV_8UC1, cv::Scalar( 0 ) );
 
     //declare output stream{{{
 
@@ -101,10 +101,10 @@ int main( int argc, char *argv[] )
             cv::resize( inputImg, inputImg, cv::Size( inputImg.cols / 2, inputImg.rows / 2 ) );
         }
         cv::Mat inputBlur;
-        cv::blur( inputImg, inputBlur, cv::Size( 4, 4 ) );
-        bsgmm.updateFrame( inputBlur.ptr(), outputImg.ptr() );
+        cv::GaussianBlur( inputImg, inputBlur, cv::Size( 5, 5 ), 0, 0 );
+        bsgmm.updateFrame( inputBlur.ptr(), outputMask.ptr() );
         cv::Mat outputMorp;
-        cv::morphologyEx( outputImg, outputMorp, CV_MOP_CLOSE, getStructuringElement( cv::MORPH_RECT, cv::Size( 5, 5 ) ) );
+        cv::morphologyEx( outputMask, outputMorp, CV_MOP_CLOSE, getStructuringElement( cv::MORPH_RECT, cv::Size( 5, 5 ) ) );
 
         // draw rect print words on img for debug {{{
         findRect rect( inputImg, outputMorp );
@@ -121,12 +121,12 @@ int main( int argc, char *argv[] )
             rectangle( inputImg, boundRect[i].tl(), boundRect[i].br(), cv::Scalar( 0, 0, 255 ), 2, 8, 0 );
         }
 
-        sprintf( str, "Frame:%d", ( int )capture.get( CV_CAP_PROP_POS_FRAMES ) );
+        sprintf( str, "Frame:%d time:%ds", ( int )capture.get( CV_CAP_PROP_POS_FRAMES ), ( int )( capture.get( CV_CAP_PROP_POS_FRAMES ) / FPS ) );
         putText( inputImg, str, cv::Point( 450, inputImg.rows - 20 ), cv::FONT_HERSHEY_PLAIN, 2,  cv::Scalar( 0, 0, 255 ), 2 );
         //}}}
 
         cv::imshow( "video", inputImg );
-        cv::imshow( "GMM", outputImg );
+        cv::imshow( "GMM", outputMask );
         cv::imshow( "inputBlur", inputBlur );
         cv::imshow( "outputMorp", outputMorp );
 
