66533d6783111ca2b5ae02d05a049ead27de70e6
Daniel <daniel78761215@gmail.com>
2016-08-17 01:45:35 -0700

    fix bug

--- PerspectiveTransform/main.cpp
+++ PerspectiveTransform/main.cpp
@@ -13,21 +13,22 @@ const int nOffset = 180; // 設定位移幅度
 
 int main( int argc, char *argv[] )
 {
+    cv::Point2f pts1[4], pts2[4];
     cv::Mat src_img = cv::imread( argv[1], 1 );
     if ( src_img.empty() )
     {
         return -1;
     }
     // 設定變換[之前]與[之後]的坐標 (左上,左下,右下,右上)
-    cv::Point2f pts1[0] = {cv::Point2f( 0, 0 )};
-    cv::Point2f pts1[1] = {cv::Point2f( 0, src_img.rows )};
-    cv::Point2f pts1[2] = {cv::Point2f( src_img.cols, src_img.rows )};
-    cv::Point2f pts1[3] = {cv::Point2f( src_img.cols, 0 )};
+    pts1[0] = {cv::Point2f( 0, 0 )};
+    pts1[1] = {cv::Point2f( 0, src_img.rows )};
+    pts1[2] = {cv::Point2f( src_img.cols, src_img.rows )};
+    pts1[3] = {cv::Point2f( src_img.cols, 0 )};
 
-    cv::Point2f pts2[0] = {cv::Point2f( 0, 0 )};
-    cv::Point2f pts2[1] = {cv::Point2f( 0 + nOffset, src_img.rows )};
-    cv::Point2f pts2[2] = {cv::Point2f( src_img.cols - nOffset, src_img.rows )};
-    cv::Point2f pts2[3] = {cv::Point2f( src_img.cols, 0 )};
+    pts2[0] = {cv::Point2f( 0, 0 )};
+    pts2[1] = {cv::Point2f( 0 + nOffset, src_img.rows )};
+    pts2[2] = {cv::Point2f( src_img.cols - nOffset, src_img.rows )};
+    pts2[3] = {cv::Point2f( src_img.cols, 0 )};
 
     // 透視變換行列計算
     cv::Mat perspective_matrix = cv::getPerspectiveTransform( pts1, pts2 );
