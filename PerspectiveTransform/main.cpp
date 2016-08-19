161c67d2b84526874fa256c6c5ff2b5f8ba9c07f
Daniel <daniel78761215@gmail.com>
2016-08-18 23:51:34 -0700

    4-2.png transform

--- PerspectiveTransform/main.cpp
+++ PerspectiveTransform/main.cpp
@@ -6,9 +6,12 @@
     Blog: https://cg2010studio.wordpress.com/
 */
 #include <iostream>
+#include <stdio.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+
+using namespace std;
 const int nOffset = 180; // 設定位移幅度
 
 int main( int argc, char *argv[] )
@@ -20,15 +23,15 @@ int main( int argc, char *argv[] )
         return -1;
     }
     // 設定變換[之前]與[之後]的坐標 (左上,左下,右下,右上)
-    pts1[0] = {cv::Point2f( 0, 0 )};
-    pts1[1] = {cv::Point2f( 0, src_img.rows )};
-    pts1[2] = {cv::Point2f( src_img.cols, src_img.rows )};
-    pts1[3] = {cv::Point2f( src_img.cols, 0 )};
+    pts1[0] = {cv::Point2f( 447, 256 )};
+    pts1[1] = {cv::Point2f( 119, 280 )};
+    pts1[2] = {cv::Point2f( 750, 560 )};
+    pts1[3] = {cv::Point2f( 822, 314 )};
 
-    pts2[0] = {cv::Point2f( 0, 0 )};
-    pts2[1] = {cv::Point2f( 0 + nOffset, src_img.rows )};
-    pts2[2] = {cv::Point2f( src_img.cols - nOffset, src_img.rows )};
-    pts2[3] = {cv::Point2f( src_img.cols, 0 )};
+    pts2[0] = {cv::Point2f( 375, 225 )};
+    pts2[1] = {cv::Point2f( 305, 470 )};
+    pts2[2] = {cv::Point2f( 810, 465 )};
+    pts2[3] = {cv::Point2f( 810, 220 )};
 
     // 透視變換行列計算
     cv::Mat perspective_matrix = cv::getPerspectiveTransform( pts1, pts2 );
@@ -45,9 +48,11 @@ int main( int argc, char *argv[] )
     cv::line( src_img, pts2[2], pts2[3], cv::Scalar( 255, 0, 255 ), 2, CV_AA );
     cv::line( src_img, pts2[3], pts2[0], cv::Scalar( 255, 0, 255 ), 2, CV_AA );
 
+    cv::imwrite( "origin.jpg", dst_img );
     cv::namedWindow( "src", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO );
     cv::namedWindow( "dst", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO );
     cv::imshow( "src", src_img );
     cv::imshow( "dst", dst_img );
+
     cv::waitKey( 0 );
 }
