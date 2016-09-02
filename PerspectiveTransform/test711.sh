6d3a045afeb25976c58e1b30e8d3d10cb18bdc29
scps950707 <scps950707@gmail.com>
2016-09-02 23:40:40 +0800

    mv PerspectiveTransform to opencvlib

--- PerspectiveTransform/test711.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd build
-make || exit
-cd ..
-./build/PerspectiveTransform -s 7-11.jpg
--- PerspectiveTransform/testkymco.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd build
-make || exit
-cd ..
-./build/PerspectiveTransform -k kymco.jpg
