4b94adfd24c5dca57a2f137c5bbb3bb001c4ba84
scps950707 <scps950707@gmail.com>
2016-09-04 16:18:22 +0800

    add aviWriter interface

---
+++ avi.cpp
@@ -0,0 +1,19 @@
+#include "avi.hpp"
+
+aviWriter::aviWriter( string path, int fps, cv::Size vidSiz )
+{
+    this->path = path;
+    this->fps = fps;
+    this->vidSiz = vidSiz;
+    this->writer.open( path, CV_FOURCC( 'D', 'I', 'V', 'X' ), fps, vidSiz );
+}
+
+void aviWriter::operator << ( cv::Mat mat )
+{
+    CV_Assert( mat.size() == this->vidSiz );
+    if ( mat.channels() == 1 )
+    {
+        cv::cvtColor( mat, mat, CV_GRAY2RGB );
+    }
+    this->writer << mat;
+}
---
+++ avi.hpp
@@ -0,0 +1,19 @@
+#ifndef __AVI_HPP__
+#define __AVI_HPP__
+
+#include "header.hpp"
+
+class aviWriter
+{
+public:
+    aviWriter( string path, int fps, cv::Size vidSiz );
+    void operator << ( cv::Mat mat );
+private:
+    string path;
+    int fps;
+    cv::Size vidSiz;
+    cv::VideoWriter writer;
+};
+
+
+#endif
